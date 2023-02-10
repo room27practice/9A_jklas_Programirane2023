0f2a1727600462da0249cf8f7f6e795c7aa21474
a <b>
2023-02-10 12:49:35 +0200

    Created First Program (Input Reading Functionality)

---
+++ Programirane/9AProgramirane/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input 3 words!");
+        Console.Write("Input 1 word: ");
+        string word1 = Console.ReadLine();
+        Console.Write("Input 2 word: ");
+        string word2 = Console.ReadLine();
+        Console.Write("Input 3 word: ");
+        string word3 = Console.ReadLine();
+
+    }
+
+
+
+}
+
+
+
+
