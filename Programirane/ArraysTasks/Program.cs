c2147d7366ab4f095136dabe7893844cf709ca52
a <b>
2023-02-17 13:16:35 +0200

    Added Second task from Arrays

---
+++ Programirane/ArraysTasks/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ArraysTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] row1 = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            string[] row2 = Console.ReadLine()
+              .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            for (int i = 0; i < row2.Length; i++)
+            {
+                if (row1.Contains(row2[i]))
+                {
+                    Console.Write(row2[i] +" ");
+                }
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
